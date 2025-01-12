bbc146fc1f6680c6c3ffb90663829c86ceb4cc23
Santhosh kumar <devsandy12@gmail.com>
2025-01-12 20:14:11 +0530

    creds update

--- config.py
+++ config.py
@@ -43,3 +43,12 @@ db_config = {
     'password': 'root',
     'database': 'hastar_reloaded'
 }
+
+flat_trade_totp_token = 'PO6I7H6SZBB757QE63R6342672P2Y4J2'
+flat_trade_client_code = 'FZ05439'
+flat_trade_password = 'Sandy@#123'
+flat_trade_api_key = '33a9ffe1e7b747dd8ddc8a8c2d89c9ca'
+flat_trade_api_secret = '2025.e184959908cf43c7a3bacff59f679b7afab000543b25c293'
+headerJson = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36",
+    "Referer": "https://auth.flattrade.in/"}
